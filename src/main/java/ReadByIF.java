dc9518f001d5947b0646edb954a01335838125e8
joonhong <qkrwns394@naver.com>
2021-03-18 17:23:09 +0900

    0318_01

--- src/main/java/ReadByIF.java
+++ src/main/java/ReadByIF.java
@@ -13,6 +13,7 @@ import org.apache.spark.sql.catalyst.expressions.GenericRow;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
+import org.codehaus.janino.Java;
 import scala.Tuple2;
 
 
@@ -36,6 +37,7 @@ public class ReadByIF {
 
         JavaPairRDD<LongWritable, FlightDataWritable> lines = sc.newAPIHadoopFile(args[0], FlightDataInputFormat.class, LongWritable.class, FlightDataWritable.class, conf);
 
+        // JavaRDD 객체 생성
         JavaRDD<Row> fulldata = lines.map(v1 -> {
             FlightDataWritable flightDataWritable = v1._2;
             String count = flightDataWritable.getCount();
@@ -44,31 +46,69 @@ public class ReadByIF {
             return new GenericRow(new Object[]{dest_country_name, origin_country_name, count});
         });
 
-//        FlightDataWritable head = fulldata.first();
-//
-//        JavaRDD<FlightDataWritable> data_nohead = fulldata.filter(v1 -> v1 != head);
+        // 헤더 정보 제거
+        Row header = fulldata.first();
+
+        /**
+         * Scala Code : var onlydata = fulldata.filter(v1 => v1 != header)
+         * Java는 !=로 값을 비교할 수 없으므로 equals()를 사용하여
+         * 아래와 같이 사용하며 주석의 내용과 같은 의미임
+         */
+
+        JavaRDD<Row> onlydata = fulldata.filter(v1 -> {
+            return !v1.equals(header);
+                });
+
+//        JavaRDD<Row> onlydata = fulldata.filter(v1 -> {
+//            if (v1.equals(header)) {
+//                return false;
+//            }else{
+//                return true;
+//            }
+//        });
 
         StructField field1 = DataTypes.createStructField("DEST_COUNTRY_NAME", DataTypes.StringType, true);
-        StructType schema = new StructType(
-//                new StructField[]{
-//                        field1,
-//                        DataTypes.createStructField("ORIGIN_COUNTRY_NAME", DataTypes.StringType, true),
-//                        DataTypes.createStructField("Count", DataTypes.StringType, true)
-//                }
-        );
-        schema.add(field1);
-        schema.add(DataTypes.createStructField("ORIGIN_COUNTRY_NAME", DataTypes.StringType, true));
-        schema.add(DataTypes.createStructField("Count", DataTypes.StringType, true));
-//
-        SQLContext spark = new SQLContext(sc);
-//        Dataset<Row> df = sqc.createDataFrame();
-        Dataset<Row> df = spark.createDataFrame(fulldata, schema);
-//        Dataset<Row> df = spark.createDataFrame(fulldata, AB.class);
+        StructField field2 = DataTypes.createStructField("ORIGIN_COUNTRY_NAME", DataTypes.StringType, true);
+        StructField field3 = DataTypes.createStructField("Count", DataTypes.StringType, true);
 
-        for (Tuple2<LongWritable, FlightDataWritable> line : lines.take(10)) {
-            FlightDataWritable fdw = line._2;
-            System.out.println(fdw.getORIGIN_COUNTRY_NAME() + " >> " + fdw.getDEST_COUNTRY_NAME() + " :: " + fdw.getCount());
-        }
+        /**
+         * Schema 생성방법 1
+         */
+//        StructType schema = new StructType();
+//
+//        schema.add(field1);
+//        schema.add(field2);
+//        schema.add(field3);
+
+        /**
+         * Schema 생성방법 2
+         */
+        StructType schema = new StructType(
+                new StructField[]{
+                        field1,
+                        field2,
+                        field3
+                }
+        );
+
+        /**
+         * Schema 생성방법 3
+         * Schema 내용들을 담은 Class를 만들어 아래와 같이 DF 생성가능
+         */
+
+//        Dataset<Row> df = spark.createDataFrame(fulldata, Schema.class);
+
+
+        SQLContext spark = new SQLContext(sc);
+
+        Dataset<Row> df = spark.createDataFrame(onlydata, schema);
+
+        df.show();
+
+//        for (Tuple2<LongWritable, FlightDataWritable> line : lines.take(10)) {
+//            FlightDataWritable fdw = line._2;
+//            System.out.println(fdw.getORIGIN_COUNTRY_NAME() + " >> " + fdw.getDEST_COUNTRY_NAME() + " :: " + fdw.getCount());
+//        }
 
     }
 }
